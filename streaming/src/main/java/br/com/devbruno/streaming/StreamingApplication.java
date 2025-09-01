135c27220a6ce8b7bdb09fbb034fbad23b3472ab
Bruno Oliveira <brunootavioliveira@gmail.com>
2025-09-01 19:23:41 -0300

    Add files via upload

---
+++ streaming/src/main/java/br/com/devbruno/streaming/StreamingApplication.java
@@ -0,0 +1,21 @@
+package br.com.devbruno.streaming;
+
+import br.com.devbruno.streaming.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=96159931");
+		System.out.println(json);
+	}
+}
